ab9daa80fb5773fc4f277d4a54280f931005d6b9
satyantazi <43914228+satyantazi@users.noreply.github.com>
2021-09-03 11:30:20 +0530

    files added

---
+++ main.js
@@ -0,0 +1,90 @@
+let c = document.getElementById('my-canvas');
+let ctx = c.getContext('2d');
+
+
+let loadImage = (src, callback) => {
+    let img = document.createElement('IMG');
+    img.onload = () => callback(img);
+    img.src = src;
+}
+
+let imagePath = (frameNumber, animation) => `images/${animation}/${frameNumber}.png`
+
+
+let frames = {
+    idle: [1, 2, 3, 4, 5, 6, 7, 8],
+    kick: [1, 2, 3, 4, 5, 6, 7],
+    punch: [1, 2, 3, 4, 5, 6, 7]
+}
+
+let loadImages = (callback) => {
+    let images = { idle: [], kick: [], punch: [] };
+    let imagesToLoad = 0;
+
+    ['idle', 'kick', 'punch'].forEach(animation => {
+        let animationFramees = frames[animation];
+        imagesToLoad = imagesToLoad + animationFramees.length;
+
+        animationFramees.forEach(frameNumber => {
+            let path = imagePath(frameNumber, animation);
+
+            loadImage(path, (image) => {
+                images[animation][frameNumber - 1] = image;
+                imagesToLoad -= 1;
+
+                if (imagesToLoad === 0) {
+                    callback(images);
+                }
+            })
+        })
+    })
+}
+
+let animate = (ctx, images, animation, cb) => {
+    images[animation].forEach((img, i) => {
+        setTimeout(() => {
+            ctx.clearRect(0, 0, 500, 500);
+            ctx.drawImage(img, 0, 0, 500, 500);
+        }, i * 100);
+    });
+
+    setTimeout(cb, images[animation].length * 100);
+};
+
+loadImages((images) => {
+    let queuedAnimations = [];
+
+
+    let aux = () => {
+        let selectedAnimation;
+
+        if (queuedAnimations.length === 0) {
+            selectedAnimation = 'idle';
+        } else {
+            selectedAnimation = queuedAnimations.shift();
+        }
+
+        animate(ctx, images, selectedAnimation, aux);
+    }
+
+    aux();
+
+
+    document.getElementById('kick').onclick = () => {
+        queuedAnimations.push('kick');
+    }
+
+    document.getElementById('punch').onclick = () => {
+        queuedAnimations.push('punch');
+    }
+
+    document.addEventListener('keyup', event => {
+        const key = event.key;
+
+        if (key === 'ArrowLeft') {
+            queuedAnimations.push('kick');
+        } else if (key === 'ArrowRight') {
+            queuedAnimations.push('punch');
+        }
+    })
+})
